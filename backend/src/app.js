4a04e4f6bc8488dfefb801746aa2b59b98eba5d4
Stella40k <bernardstella0704@gmail.com>
2025-09-04 16:26:29 -0300

    feat: configuracion de las variables de entorno

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 
-export const app = express();
-
 app.use(express.json());
 app.use(cors());
+
+const app = express();
+
+app.listen(prompt, async() =>{
+    console.log(`server corriendo en ${PORT}`)
+    try {
+        await seq
+    } catch (error) {
+        
+    }
+})
--- backend/src/configs/config.env.js
+++ backend/src/configs/config.env.js
@@ -2,12 +2,19 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-const configEnv = DB()
-PORT=3005
-DB_NAME= HemoApp
-DB_USER=root
-DB_PASSWORD=""
-DB_HOST=localhost
-DB_DIALECT=mysql
+export const PORT = process.env.PORT;
+export const JWT_SECRET = process.env.JWT_SECRET;
 
-export default configEnv
+export const DB_CONFIG = {
+    host: process.env.HOST,
+    port:process.env.DB_PPORT,
+    user:process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database: prpcess.env.DB_NAME
+};
+
+/* //por si lo necesito en otro lado
+export const DB_HOST = process.env.DB_HOST;
+export const DB_USER = process.env.DB_USER;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_NAME = process.env.DB_NAME; */
