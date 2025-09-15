b12babaa5294be52504370aa1595056f0d67afe7
Stella40k <bernardstella0704@gmail.com>
2025-09-15 16:47:33 -0300

    commit de respañdo

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,2 +1,16 @@
-import User from "../models/user.model.js"
+import {User} from "../models/user.model.js"
 import { generateToken } from "../utils/generateToken.js"
+
+export const register = async(req, res)=>{
+    try {
+        const{email, password, coordinates}= req.body;
+        
+        //validar si ya existe el usuario
+        const userExists = await User.findOne({email});
+        if(userExists){
+            return res.status(400).json({error: "El usuario ya existe"})
+        }
+    } catch (error) {
+        
+    }
+}
