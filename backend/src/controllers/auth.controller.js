b5ae96c0f48bc85d559f209baa3f2c761dd0f7f8
Stella40k <bernardstella0704@gmail.com>
2025-09-16 10:08:14 -0300

    feat: controladores para el login, registro y logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -10,7 +10,76 @@ export const register = async(req, res)=>{
         if(userExists){
             return res.status(400).json({error: "El usuario ya existe"})
         }
+
+        //crear un nuevo usuario con ubicacuon
+        const user = await User.create({
+            email,
+            passwordHash: password,
+            location:{
+                type: "Point",
+                coordinates: coordinates || [0, 0] //[longi, latit]
+            }
+        });
+
+        //generacion de JWT
+        const token = generateToken(user._id);
+        res.status(201).json({
+            ok: true,
+            msg: "Usuario registrado",
+            token,
+            user:{
+                id: user._id,
+                email: user.email,
+                coordinates: user.location.coordinates
+            }
+        });
     } catch (error) {
-        
+        res.status(500).json({
+            ok: false,
+            msg: "Ups! Error interno del servidor",
+            data: error
+        })
     }
-}
+};
+export const login = async(req, res)=>{
+    try {
+        const{email, password}= req.body;
+        
+        //buscamos el user y verificamos credenciales
+        const user = await User.findOne({ email });
+        if(!user || !(await user.matchPassword(password))){
+            return res.status(401).json({
+                ok: false,
+                msg: "Credenciales incorrectas",
+                data: error
+            });
+        }
+
+        //generamos un nuevo token
+        const token = generateToken(user._id);
+        res.json({
+            ok: true,
+            msg: "Login exitoso",
+            token,
+            user:{
+                id: user._id,
+                email: user.email,
+                bloodType: user.bloodType,
+                coordinates: user.location.coordinates,
+                status: user.status
+            }
+        });
+    } catch (error) {
+        res.status(500).json({
+            ok: false,
+            msg: "Ups! Ocurrio un error",
+            data: error
+        });
+    }
+};
+export const logout = (req, res)=>{
+    res.json({
+        ok: true,
+        msg:"Logout exitoso"
+    });
+};
