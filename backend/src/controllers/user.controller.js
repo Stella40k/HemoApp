8ea641626c850adb6f317d39e3895766955b5c64
Stella40k <bernardstella0704@gmail.com>
2025-09-16 13:00:47 -0300

    feat: controladores para usuarios

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,4 @@
+import { InvalidConnectionError } from "sequelize";
 import { User } from "../models/user.model.js";
 
 export const getUsers = async(req, res)=>{
@@ -44,15 +45,57 @@ export const User = async(req, res)=>{
 };
 export const updateUser = async(req, res)=>{
     try {
-        
+        const{email, bloodType, coordinates, status, inactivetyReason} = req.body;
+        const user = await User.findByIdAndUpdate(
+            re.params.id,
+            {email, bloodType, coordinates, status, inactivetyReason},
+            { new: true, runValidators: true}
+        );
+        if(!user){
+            return res.status(404).json({
+                ok: false,
+                msg: "Usuario no encontrado"
+            });
+        }
+        res.status(200).json({
+            ok: true,
+            msg:"Usuario actualizado",
+            data: user
+        });
     } catch (error) {
-        
+       res.status(500).json({
+        ok: false,
+        msg: "Error al actualizar el usuario",
+        data: error
+       });
     }
 };
+//eliminacion logica del user
 export const deactivateUser = async(req, res)=>{
     try {
-        
+       const user = await User.findByIdAndUpdate(
+        req.params.id,
+        //Descansando es para avisar que el usuario no esta en momento de donar, pero podria seguir
+        //activo en la app, ver como se vera esto o como hacer esto
+        {status: "Descansando", inactivetyReason: req.body.reason || "Desactivado por admin"},
+        { new: true}
+       );
+       if(!user){
+        return res.status(404).json({
+            ok: false,
+            msg: "Usuario no encontrado"
+        });
+       }
+       res.status(200).json({
+        ok: true,
+        msg: "Usuario desactivado",
+        data: user
+       });
     } catch (error) {
-        
+        res.status(500).json({
+            ok: false,
+            msg: "Error al desactivar el usuario",
+            data: error
+        });
     }
 };
