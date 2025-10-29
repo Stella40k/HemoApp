fa43e75f30b0a3dc5bbf59bf50ffdcaa42980cfe
Stella40k <bernardstella0704@gmail.com>
2025-10-29 11:19:37 -0300

    feat: validaciones para instituciones

---
+++ backend/src/middlewares/validations/institution.validation.js
@@ -0,0 +1,42 @@
+import {body, validationResult} from 'express-validator';
+export const createInstitutionValidation=[
+    body('legalName')
+    .notEmpty().withMessage("El nombre legal es obligatorio")
+    .isLength({max: 200}).withMessage("Maximo 200 caracteres"),
+    body('cuit')
+    ,matches(/^\d{11}$/).withMessage("El CUIT debe tener 11 digitos"),
+    body(contact.email)
+    .optional()
+    .isEmail().withMessage("Email invalido"),
+    body('addres.city')
+    .notEmpty().withMessage("La ciudad de procedencia es obligatorio"),
+    body('location.coordinates')
+    .isArray({min: 2, max: 2}).withMessage("Debe ingresar 2 coordenadas")
+    .custom((coords)=>{
+        const [lng, lat]=coords;
+        return lng >= -180 && lng <= 180 && lat >= -90 && lat <= 90;
+    }).withMessage("Coordenadas invalidas"),
+    body('institutionType')
+    .isIn([
+        "hospital_publico",
+            "hospital_privado", 
+            "clinica",
+            "centro_salud",
+            "banco_sangre",
+            "laboratorio",
+            "campaña_movil",
+            "otro"
+    ]).withMessage("Tipo de institucion invalida"),
+    (req, res, next)=>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                ok: false,
+                msg: "Errores de validacion",
+                errors: errors.array()
+                
+            });
+        }
+        next();
+    }
+];
