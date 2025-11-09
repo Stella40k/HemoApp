9457f2d6daebafc8a4b335d703f45dc78511e791
Stella40k <bernardstella0704@gmail.com>
2025-11-09 12:30:11 -0300

    fix: token y refresh token del login

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,34 +1,34 @@
 import express from "express";
 import {
-    register,
-    login,
-    getMyProfile,
-    logout,
-    verifyEmail,
-    refreshToken
+  register,
+  login,
+  getMyProfile,
+  logout,
+  verifyEmail,
+  // refreshToken
 } from "../controller/auth.controller.js";
 import {
-    authenticateToken,
-    validateRefreshToken
+  authenticateToken,
+  validateRefreshToken,
 } from "../middlewares/auth.middleware.js";
-import{
-    registerValidation,
-    loginValidation
-} from "../middlewares/validations/auth.validation.js"
-import{
-    authRateLimite,
-    loginRateLimite
-} from "../middlewares/rateLimiter.middleware.js"
+import {
+  registerValidation,
+  loginValidation,
+} from "../middlewares/validations/auth.validation.js";
+import {
+  authRateLimite,
+  loginRateLimite,
+} from "../middlewares/rateLimiter.middleware.js";
 
 const authRouter = express.Router();
 //rutas publicas
 authRouter.post("/auth/register", authRateLimite, registerValidation, register);
-authRouter.post("/auth/login" , loginRateLimite, loginValidation, login);
+authRouter.post("/auth/login", loginRateLimite, loginValidation, login);
 authRouter.get("/auth/verify-email/:token", verifyEmail);
 
 //rutas protegidas(requieren token)
 authRouter.get("/auth/me", authenticateToken, getMyProfile);
 authRouter.post("/auth/logout", authenticateToken, logout);
-authRouter.post("/auth/refresh-token", validateRefreshToken, refreshToken);
+//authRouter.post("/auth/refresh-token", validateRefreshToken, refreshToken);
 
 export default authRouter;
