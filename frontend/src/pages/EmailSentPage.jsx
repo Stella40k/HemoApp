e829104cde1d91dff602d39f2ff3d54b5ec66ac3
Stella40k <bernardstella0704@gmail.com>
2025-11-11 15:57:25 -0300

    feat: pagina de espera para verificacion de email

---
+++ frontend/src/pages/EmailSentPage.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Mail } from "lucide-react"; //importa dos componentes de una libreria de iconos
+import { Link } from "react-router-dom";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+export default function EmailSentPage() {
+  return (
+    <div className="min-h-scree bg-gray-50 flex items-center justify-center p-6">
+      <Card className="w-full max-w-md text-center shadow-2xl">
+        <CardHeader>
+          <div className="mx-auto mb-4 w-16 h-16 bg-red-100 rounded-full flex items-center justify-center">
+            <Mail className="w-8 h-8 text-[#e74c3c]" />
+          </div>
+          <CardTitle className="text-2xl front-bold text-gray-800">
+            ¡Falta un ultimo paso! Revisa tu bandeja de entrada
+          </CardTitle>
+          <CardDescription className="mt-2 text-gray-600">
+            Hemos enviado un enlace de verificacion a tu correo electronico.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-6">
+          <p className="text-sm text-gray-700">
+            La verificacion es necesaria para completar el registro y puedas
+            comenzar a salvar vidas. Por favor, haz clic en el enlace para{" "}
+            <span className="font-semibol text-red-600">activar perfil</span>
+          </p>
+          <div className="bg-yellow-50 border l-4 border-yellow-400 p-3 text-sm text-left">
+            <p className="font-semibold">¿No lo recibiste?</p>
+            <p className="text-red-800">
+              Revisa tu carpeta de spam o correo no deseado.
+            </p>
+          </div>
+          <Link to="/"></Link>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
